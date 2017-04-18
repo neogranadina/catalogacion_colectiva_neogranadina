fc45f9f0fab01debc52d8d7c024d20ba1837e6a7
Jairo Antonio Melo Flórez <jairomelo@users.noreply.github.com>
2017-04-18 13:49:16 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+//Por favor, diligencie la siguiente información:
+
+$servername = "localhost";
+$username = "root";
+$password = "xxxxxx";
+$dbname = "neogranadina";
+
+//Conectar al servidor
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Comprobar conexión
+if (!$conn) {
+    die("Fallo en la conexión: " . mysqli_connect_error());
+}
+
+// Definir ruta absoluta
+
+define('RAIZ_APLICACION', dirname(__FILE__))
+
+?>
